cd03305c1bcfb6ac76b57671e93db4316689590b
서현 <105104335+horang-e@users.noreply.github.com>
2023-03-06 17:47:50 +0900

    Create loginLogic.js

---
+++ loginLogic.js
@@ -0,0 +1,13 @@
+import {restApi} from '#apis';
+import {setStorage} from '#utils/rn.async.storage';
+import {numreg, pwreg} from '#commons/regulation';
+
+export const sendLogin = async ({data, navigation}) => {
+  await restApi.post('api', data).then(res => {
+    const token = res.data.payload.access_token;
+    const refreshtoken = res.data.payload.refresh_token;
+    setStorage('token', token);
+    setStorage('refreshToken', refreshtoken);
+    navigation.navigate('main page');
+  });
+};
