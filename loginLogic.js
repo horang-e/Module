4f1c2bc4945e7ff18775d9d516c9fe34692cbbcb
서현 <105104335+horang-e@users.noreply.github.com>
2023-03-06 18:44:06 +0900

    Update loginLogic.js

--- loginLogic.js
+++ loginLogic.js
@@ -1,6 +1,6 @@
 import {restApi} from '#apis';
 import {setStorage} from '#utils/rn.async.storage';
-
+import {numreg, pwreg} from '#commons/regulation';
 
 export const sendLogin = async ({data, navigation}) => {
   await restApi.post('api', data).then(res => {
@@ -9,5 +9,26 @@ export const sendLogin = async ({data, navigation}) => {
     setStorage('token', token);
     setStorage('refreshToken', refreshtoken);
     navigation.navigate('main page');
+  });
+};
+
+export const dupId = async ({text, error, setError}) => {
+  await restApi
+    .post('api', text)
+    .then(res => setError({...error, iddup: true}))
+    .catch(err => setError({...error, iddup: false}));
+};
+
+export const pwReg = ({text, setError, error}) => {
+  if (pwreg(text)) {
+    setError({...error, pwreg: true});
+  } else {
+    setError({...error, pwreg: false});
+  }
+};
+
+export const setSignIn = async ({data, navigation}) => {
+  await restApi.post('api', data).then(res => {
+    navigation.navigate('로그인');
   });
 };
